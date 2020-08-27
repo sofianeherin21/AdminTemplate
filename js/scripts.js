06b1aa6508a31195cc94d015ad73ca88f70033bc
Sofia Neherin <sofia.neherin@gmail.com>
2020-08-27 21:55:32 +0600

    Collapsible Modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,49 @@
-var coll = document.getElementsByClassName("card-header");
-		var i;
+var coll1 = document.getElementById("row1head");
+var content1 = document.getElementById("row1");
+var content2 = document.getElementById("row2");
+var content3 = document.getElementById("row3");
 
-		for (i = 0; i < coll.length; i++) {
-			
-		  coll[i].addEventListener("click", function() {
-		    console.log(typeof i);
-		    console.log(`${i-1}`)
-		    var content = document.getElementsByClassName("collapse");
+coll1.addEventListener("click", function() {
 
-		    if (content[0].style.display === "block") {
-		      content[0].style.display = "none";
-		    } else {
-		      content[0].style.display = "block";
-		    }
-		  });
-		}
+
+    if (content1.style.display === "block") {
+      content1.style.display = "none";
+    } else {
+      content2.style.display = "none";
+      content1.style.display = "block";
+      content3.style.display = "none";
+    }
+
+});
+
+
+var coll2 = document.getElementById("row2head");				
+coll2.addEventListener("click", function() {
+
+
+
+    if (content2.style.display === "block") {
+      content2.style.display = "none";
+    } else {
+      content2.style.display = "block";
+      content1.style.display = "none";
+      content3.style.display = "none";
+    }
+
+});
+
+
+var coll3 = document.getElementById("row3head");				
+coll3.addEventListener("click", function() {
+
+
+
+    if (content3.style.display === "block") {
+      content3.style.display = "none";
+    } else {
+      content2.style.display = "none";
+      content1.style.display = "none";
+      content3.style.display = "block";
+    }
+
+});
